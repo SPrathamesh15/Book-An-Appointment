1f87647de32d115dce57935b267cb2203859ffc8
SPrathamesh15 <sprathamesh354@gmail.com>
2023-10-17 12:14:27 +0530

    Lets store the users in the right format

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ function setItem(e){
     e.preventDefault()
     // Get Input value
     var newItem = document.getElementById('item').value;
-    // Get description value
     var newdesc = document.getElementById('description').value;
-    localStorage.setItem(newItem, newdesc)
-    console.log('value is added to localStorage')
+    // Get description value
+    let myObj = {
+        firstName: newItem,
+        lastName: newdesc
+    }
+    let myObjstr = JSON.stringify(myObj)
+    localStorage.setItem('myObj', myObjstr)
+    let myObjdestr = JSON.parse(localStorage.getItem('myObj'))
+    console.log('deseriliazed sting from localStorage' + myObjdestr)
+    console.log('value is added to localStorage ' +myObjstr)
 }
