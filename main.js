666c52fa670681e4ef078d91ce35151f6d345e90
SPrathamesh15 <sprathamesh354@gmail.com>
2023-11-01 11:46:44 +0530

    Saving the user Details on Crud Crud

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-var form = document.getElementById('addForm')
-var itemList = document.getElementById('items')
-// set event
-form.addEventListener('submit', setItem)
-// Add event
-form.addEventListener('submit', addItem)
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+// Add a single event listener to handle form submission
+form.addEventListener('submit', handleFormSubmission);
+
 // delete event
 itemList.addEventListener('click', removeItem);
-// edit event
-itemList.addEventListener('click', editItem);
+itemList.addEventListener('click', editItem); // Add event listener for edit
+
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are You Sure? You want to delete this item?')) {
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+            // Removing corresponding userData from the localStorage
+            var email = document.getElementById('email').value;
+            var userData = JSON.parse(localStorage.getItem(email));
+            if (userData) {
+                // Updating the localStorage after deleting the user
+                localStorage.removeItem(email);
+            }
+        }
+    }
+}
 
 function editItem(e) {
-    if (e.target.classList.contains('edit')) {
+    if (e.target.classList.contains('editbutton')) {
         var li = e.target.parentElement;
         var email = document.getElementById('email').value;
         var userData = JSON.parse(localStorage.getItem(email));
@@ -31,93 +46,62 @@ function editItem(e) {
     }
 }
 
-
-
-// Remove item
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-      if(confirm('Are You Sure? You want to delete this item?')){
-        var li = e.target.parentElement;
-        itemList.removeChild(li);
-        // Removing corresponding userData from the localStorage
-        var emails = document.getElementById('email').value;
-        var userData = JSON.parse(localStorage.getItem(emails));
-        if (userData){
-            // updating the localStorage after deleting the user
-            localStorage.removeItem(emails);
-        }
-      }
-    }
-  }
-
-//creating a function to show details on the page itself
-function addItem(e){
+function handleFormSubmission(e) {
     e.preventDefault();
-    // Get Input value
     var newItem = document.getElementById('item').value;
-    // Get description value
     var newdesc = document.getElementById('description').value;
-    // Get Email value
     var email = document.getElementById('email').value;
-    // Get Phone value
     var phone = document.getElementById('phone').value;
-    // create del button element 
-    var deleteBtn = document.createElement('button');
-    // Add class to del button
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    // Append text node
-    deleteBtn.appendChild(document.createTextNode('X'));
-    // create edit btn
-    var editBtn = document.createElement('button');
-    // add class to edit btn
-    editBtn.className = 'btn btn-primary btn-sm float-right edit'
-    // Append text node
-    editBtn.appendChild(document.createTextNode('Edit'))
-    
-    // create a new li element
+
     var li = document.createElement('li');
-    // Add class
-    li.className = 'list-group-item'
-    // Add text node with input value
-    var space = ' '
-    var bigSpace = ' - '
-    li.appendChild(document.createTextNode('FullName: '+newItem));
+    li.className = 'list-group-item';
+
+    var space = ' ';
+    var bigSpace = ' - ';
+
+    var fullName = 'FullName: ' + newItem;
+    var emailText = 'Email: ' + email;
+    var phoneText = 'Phone Number: ' + phone;
+
+    li.appendChild(document.createTextNode(fullName));
     li.appendChild(document.createTextNode(space));
     li.appendChild(document.createTextNode(newdesc));
     li.appendChild(document.createTextNode(bigSpace));
-    li.appendChild(document.createTextNode('Email: '+email));
+    li.appendChild(document.createTextNode(emailText));
     li.appendChild(document.createTextNode(bigSpace));
-    li.appendChild(document.createTextNode("Phone Number: "+ phone));
-    
-    itemList.appendChild(li);
-    // Append btn to li
-    li.appendChild(deleteBtn);
-    // append btn to li
-    li.appendChild(editBtn)
-}
+    li.appendChild(document.createTextNode(phoneText));
+    li.appendChild(document.createTextNode(space));
 
-//creating function to add the items to localstorage
-function setItem(e){
-    e.preventDefault()
-    // Get Input value
-    var newItem = document.getElementById('item').value;
-    var newdesc = document.getElementById('description').value;
-    var emails = document.getElementById('email').value;
-    var phones = document.getElementById('phone').value;
-    // Retrieve the existing data from local storage or initialize an empty array
-    let existingData = JSON.parse(localStorage.getItem(emails)) || [];
-    // Get description value
-    let myObj = {
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    var editBtn = document.createElement('button'); // Create an edit button
+    editBtn.className = 'editbutton';
+    editBtn.appendChild(document.createTextNode('Edit'));
+
+    li.appendChild(deleteBtn);
+    li.appendChild(editBtn); // Append the edit button
+
+    itemList.appendChild(li);
+
+    var userDetails = {
         firstName: newItem,
         lastName: newdesc,
-        email: emails,
-        phone: phones
-    }
-    existingData.push(myObj);
-
-    // Store the updated array in local storage
-    localStorage.setItem(emails, JSON.stringify(existingData));
-
-    console.log('Array of objects added to local storage', existingData);
+        emailId: email,
+        phone: phone
+    };
     
+    axios.post("https://crudcrud.com/api/989bfbabc6634ee6966be6ef88ef3885/bookingAppointData"
+    , userDetails)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((err) => {
+            document.body.innerHTML = document.body.innerHTML + "<h3 style='color:red'> Something Went wrong!!!</h4>",
+            console.log(err)
+        })
+    // localStorage.setItem(email, JSON.stringify(userDetails));
+
+    console.log('User details added to crud crud!!', userDetails);
 }
