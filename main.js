c528a1b64d50f0d726361159cb755bbbd59baf7c
SPrathamesh15 <sprathamesh354@gmail.com>
2023-11-02 12:16:04 +0530

    Get the saved User Details from crudcrud.

--- main.js
+++ main.js
@@ -92,16 +92,68 @@ function handleFormSubmission(e) {
         phone: phone
     };
     
-    axios.post("https://crudcrud.com/api/989bfbabc6634ee6966be6ef88ef3885/bookingAppointData"
+    axios.post("https://crudcrud.com/api/2ca9952db08a406d8dc1c4b662b347a5/bookingAppointData"
     , userDetails)
         .then((response) => {
             console.log(response)
         })
         .catch((err) => {
-            document.body.innerHTML = document.body.innerHTML + "<h3 style='color:red'> Something Went wrong!!!</h4>",
+            document.body.innerHTML = document.body.innerHTML + 
+            "<h3 style='color:red'> Something Went wrong!!!</h4>",
             console.log(err)
         })
     // localStorage.setItem(email, JSON.stringify(userDetails));
 
     console.log('User details added to crud crud!!', userDetails);
 }
+
+document.addEventListener('DOMContentLoaded', handlePageLoad);
+
+function handlePageLoad() {
+    // Make a GET request to retrieve data
+    axios.get("https://crudcrud.com/api/2ca9952db08a406d8dc1c4b662b347a5/bookingAppointData")
+        .then((response) => {
+            // Display the data on the screen and in the console
+            showNewUserOnScreen(response.data);
+        })
+        .catch((err) => {
+            console.error('Error while fetching data:', err);
+        });
+}
+
+function showNewUserOnScreen(user) {
+    console.log(user);
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+
+    var space = ' ';
+    var bigSpace = ' - ';
+    li.appendChild(document.createTextNode(user[0].firstName));
+    li.appendChild(document.createTextNode(space));
+    li.appendChild(document.createTextNode(user[0].lastName));
+    li.appendChild(document.createTextNode(bigSpace));
+    li.appendChild(document.createTextNode(user[0].emailId));
+    li.appendChild(document.createTextNode(bigSpace));
+    li.appendChild(document.createTextNode(user[0].phone));
+    li.appendChild(document.createTextNode(space));
+
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    var editBtn = document.createElement('button'); // Create an edit button
+    editBtn.className = 'editbutton';
+    editBtn.appendChild(document.createTextNode('Edit'));
+
+    li.appendChild(deleteBtn);
+    li.appendChild(editBtn); // Append the edit button
+
+    itemList.appendChild(li);
+    // localStorage.setItem(email, JSON.stringify(userDetails));
+
+    console.log('User details retrieved from crud crud!!');
+    
+}
+
+
+
